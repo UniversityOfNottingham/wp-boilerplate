bc92bcdcb6bdfbab7abb3f0fca68a9c040a8c314
Alan Shortis <alan.shortis@mac.com>
2016-11-30 13:09:51 +0000

    Added dotenv, to store credentials across environments

--- wp-config.php
+++ wp-config.php
@@ -1,82 +1,45 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
- */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+// Register the composer auto loader.
+require __DIR__.'/vendor/autoload.php';
 
-/** MySQL database username */
-define('DB_USER', 'username_here');
+// Detect the environment.
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+// The name of the database for WordPress.
+define('DB_NAME', getenv('DB_NAME'));
 
-/** Database Charset to use in creating database tables. */
+// MySQL database username.
+define('DB_USER', getenv('DB_USER'));
+
+// MySQL database password.
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+
+// MySQL hostname.
+define('DB_HOST', getenv('DB_HOST'));
+
+// Database Charset to use in creating database tables.
 define('DB_CHARSET', 'utf8');
 
-/** The Database Collate type. Don't change this if in doubt. */
+// The Database Collate type.
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
- define('AUTH_KEY',         'put your unique phrase here');
- define('SECURE_AUTH_KEY',  'put your unique phrase here');
- define('LOGGED_IN_KEY',    'put your unique phrase here');
- define('NONCE_KEY',        'put your unique phrase here');
- define('AUTH_SALT',        'put your unique phrase here');
- define('SECURE_AUTH_SALT', 'put your unique phrase here');
- define('LOGGED_IN_SALT',   'put your unique phrase here');
- define('NONCE_SALT',       'put your unique phrase here');
+// Keys and Salts.
+define('AUTH_KEY', getenv('DB_NAME'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
 
-/**#@-*/
+// WordPress Database Table prefix.
+$table_prefix  = getenv('DB_PREFIX');
 
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
- */
+// Debug mode.
 define('WP_DEBUG', true);
 
 /** Add some new constants so we can keep WordPress in a new folder */
